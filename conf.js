2795edd57fed5d7227f15e7566782c75c98ed56f
linzjian666 <63142068+linzjian666@users.noreply.github.com>
2024-05-04 21:52:29 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,9 +29,7 @@ jsproxy_config({
     'mysite': {
       label: '当前站点',
       lines: {
-        'b.007.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
+        [location.host]: 1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -54,14 +52,13 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  // node_default: 'cfworker',
-  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: 'mysite',
+  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
    */
-  // node_acc: 'cfworker',
-  node_acc: 'mysite',
+  node_acc: 'cfworker',
 
   /**
    * 静态资源 CDN 地址
