a44eacd5b15f861e9e58075fc7fbe7d6dc0f2d73
linzjian666 <63142068+linzjian666@users.noreply.github.com>
2024-05-04 21:46:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -34,7 +34,7 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
+      label: 'cfworker',
       hidden: true,
       lines: {
         // 收费版（高权重）
@@ -52,7 +52,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'cfworker',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
