b44becc7cd6f053cc5c2470bb8bf537cc8bcfe02
linzjian666 <63142068+linzjian666@users.noreply.github.com>
2024-05-05 08:38:15 +0800

    Update and rename _worker.js to netlify.toml

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const _url = new URL(request.url);
-    _url.hostname = "jsproxy.linzjian666.workers.dev";
-    _url.protocol = "https";
-    const req = new Request(_url, request);
-    return fetch(req);
-  },
-};
